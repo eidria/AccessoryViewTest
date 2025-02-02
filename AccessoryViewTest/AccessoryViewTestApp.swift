53e6f431f4d9199fde02997523b1cb6751ecbf85
Dan Galbraith <dan@eidria.com>
2025-02-01 16:12:03 -0800

    Initial Commit

---
+++ AccessoryViewTest/AccessoryViewTestApp.swift
@@ -0,0 +1,18 @@
+////    AccessoryViewTestApp.swift
+//    AccessoryViewTest
+//
+//    Created by Dan Galbraith on 2/1/25.
+//
+//    Copyright © 2025 Eidria Inc. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct AccessoryViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessoryViewTest/ContentView.swift
@@ -0,0 +1,25 @@
+////    ContentView.swift
+//    AccessoryViewTest
+//
+//    Created by Dan Galbraith on 2/1/25.
+//
+//    Copyright © 2025 Eidria Inc. All rights reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
